bba5cbb5e8aabf44390771ab61fa1d67b35edad9
davidemornatta <davidemornatta@gmail.com>
2021-04-11 18:10:34 +0200

    feat: completed implementing PlayerController methods

--- src/main/java/it/polimi/ingsw/controller/PlayerController.java
+++ src/main/java/it/polimi/ingsw/controller/PlayerController.java
@@ -1,7 +1,6 @@
 package it.polimi.ingsw.controller;
 
 import it.polimi.ingsw.model.Resource;
-import it.polimi.ingsw.model.card.Card;
 import it.polimi.ingsw.model.card.DevelopmentCard;
 import it.polimi.ingsw.model.card.LeaderCard;
 import it.polimi.ingsw.model.card.SpecialAbilityType;
@@ -19,15 +18,12 @@ public class PlayerController {
         this.gameController = gameController;
     }
 
-    //TODO Maybe check nullity for all params
-    //TODO Send errors to player
-
     public void activateLeaderCard(Player player, LeaderCard leader) {
-        if(!gameController.isPlaying(player)) {
-            System.out.println("Not " + player.getNick() + " turn!");
+        if (!gameController.isPlaying(player)) {
+            System.out.println("Not " + player.getNick() + "'s turn!");
             return;
         }
-        if(!leader.canPlayerAfford(player)) {
+        if (!leader.canPlayerAfford(player)) {
             System.out.println(player.getNick() + " cannot afford the leader card!");
             return;
         }
@@ -40,15 +36,15 @@ public class PlayerController {
     }
 
     public void buyDevelopmentCard(Player player, DevelopmentCard developmentCard, int slot) {
-        if(gameController.getGame().getDeck().getDevelopmentCardByUuid(developmentCard.getUuid()) == null) {
+        if (gameController.getGame().getDeck().getDevelopmentCardByUuid(developmentCard.getUuid()) == null) {
             System.out.println("The requested card is not in the deck!");
             return;
         }
-        if(!developmentCard.canPlayerAfford(player)) {
+        if (!developmentCard.canPlayerAfford(player)) {
             System.out.println(player.getNick() + " cannot afford the development card!");
             return;
         }
-        if(!player.getBoard().canPlaceCardOfLevel(developmentCard.getLevel(), slot)) {
+        if (!player.getBoard().canPlaceCardOfLevel(developmentCard.getLevel(), slot)) {
             System.out.println(player.getNick() + " does not have space for this kind of card!");
             return;
         }
@@ -59,13 +55,13 @@ public class PlayerController {
         gameController.getGame().getDeck().removeBoughtCard(developmentCard);
         player.getBoard().addCard(slot, developmentCard);
 
-        if(endGame)
+        if (endGame)
             gameController.endGame();
     }
 
     public void updatePlayerDeposit(Player player, Map<Integer, List<Resource>> changes) {
-        if(!gameController.isPlaying(player)) {
-            System.out.println("Not " + player.getNick() + " turn!");
+        if (!gameController.isPlaying(player)) {
+            System.out.println("Not " + player.getNick() + "'s turn!");
             return;
         }
 
@@ -78,8 +74,8 @@ public class PlayerController {
     }
 
     public void discardLeader(Player player, LeaderCard card) {
-        if(!gameController.isPlaying(player)) {
-            System.out.println("Not " + player.getNick() + " turn!");
+        if (!gameController.isPlaying(player)) {
+            System.out.println("Not " + player.getNick() + "'s turn!");
             return;
         }
 
@@ -91,24 +87,24 @@ public class PlayerController {
     }
 
     public void useMarket(Player player, int selection, Resource whiteConversion) {
-        if(!gameController.isPlaying(player)) {
-            System.out.println("Not " + player.getNick() + " turn!");
+        if (!gameController.isPlaying(player)) {
+            System.out.println("Not " + player.getNick() + "'s turn!");
             return;
         }
 
         List<Resource> marketResults = new ArrayList<>();
-        if(selection >= 0 && selection < 4) {
+        if (selection >= 0 && selection < 4) {
             marketResults.addAll(gameController.getGame().getMarket().getColumn(selection));
             gameController.getGame().getMarket().shiftColumn(selection);
-        } else if(selection >= 4 && selection < 7) {
+        } else if (selection >= 4 && selection < 7) {
             int row = selection - 4;
             marketResults.addAll(gameController.getGame().getMarket().getRow(row));
             gameController.getGame().getMarket().shiftRow(row);
         }
 
-        for(int i = marketResults.size() - 1; i >= 0; i--) {
-            if(marketResults.get(i) == null) {
-                if(whiteConversion != null)
+        for (int i = marketResults.size() - 1; i >= 0; i--) {
+            if (marketResults.get(i) == null) {
+                if (whiteConversion != null)
                     marketResults.set(i, whiteConversion);
                 else
                     marketResults.remove(i);
@@ -119,19 +115,19 @@ public class PlayerController {
     }
 
     public void useBaseProduction(Player player, List<Resource> baseProductionInput, Resource baseProductionOutput) {
-        if(!gameController.isPlaying(player)) {
-            System.out.println("Not " + player.getNick() + " turn!");
+        if (!gameController.isPlaying(player)) {
+            System.out.println("Not " + player.getNick() + "'s turn!");
             return;
         }
-        if(baseProductionInput.size() != 2) {
+        if (baseProductionInput.size() != 2) {
             System.out.println("Wrong base production format!");
             return;
         }
 
         Map<Resource, Integer> cost = new HashMap<>();
-        if(baseProductionInput.get(0) == baseProductionInput.get(1)) {
+        if (baseProductionInput.get(0) == baseProductionInput.get(1)) {
             Resource resource = baseProductionInput.get(0);
-            if(player.getBoard().getDeposit().getAmountOfResource(resource) < 2) {
+            if (player.getBoard().getDeposit().getAmountOfResource(resource) < 2) {
                 System.out.println(player.getNick() + " does not have enough " + resource);
                 return;
             }
@@ -140,11 +136,11 @@ public class PlayerController {
         } else {
             Resource resource1 = baseProductionInput.get(0);
             Resource resource2 = baseProductionInput.get(1);
-            if(player.getBoard().getDeposit().getAmountOfResource(resource1) < 1) {
+            if (player.getBoard().getDeposit().getAmountOfResource(resource1) < 1) {
                 System.out.println(player.getNick() + " does not have enough " + resource1);
                 return;
             }
-            if(player.getBoard().getDeposit().getAmountOfResource(resource2) < 1) {
+            if (player.getBoard().getDeposit().getAmountOfResource(resource2) < 1) {
                 System.out.println(player.getNick() + " does not have enough " + resource2);
                 return;
             }
@@ -157,38 +153,70 @@ public class PlayerController {
         player.getBoard().getDeposit().addToStrongbox(baseProductionOutput);
     }
 
-    public void useProduction(Player player, Card card) {
-        if(!gameController.isPlaying(player)) {
-            System.out.println("Not " + player.getNick() + " turn!");
+    public void useDevelopmentProduction(Player player, DevelopmentCard card) {
+        if (!gameController.isPlaying(player)) {
+            System.out.println("Not " + player.getNick() + "'s turn!");
+            return;
+        }
+        if(card == null) {
+            System.out.println("The requested card is not in the deck!");
             return;
         }
 
-        Map<Resource, Integer> cost = new HashMap<>();
-
-        if (card instanceof DevelopmentCard) useDevelopmentProduction(player, (DevelopmentCard) card);
-        else if (card instanceof LeaderCard) useLeaderProduction(player, (LeaderCard) card);
-        else {
-            System.out.println("Incorrect card format");
-        }
-    }
-
-    public void useDevelopmentProduction(Player player, DevelopmentCard card) {
         Map<Resource, Integer> input = card.getProductionInput();
-        Map<Resource, Integer> output =  card.getProductionOutput();
+        Map<Resource, Integer> output = card.getProductionOutput();
 
+        for (Resource res : input.keySet()) {
+            if (player.getBoard().getDeposit().getAmountOfResource(res) < input.get(res)) {
+                System.out.println(player.getNick() + " does not have enough " + res);
+                return;
+            }
+        }
         player.getBoard().getDeposit().removeResources(input);
-        if(output.containsKey(Resource.FAITH)) {
+        if (output.containsKey(Resource.FAITH)) {
             player.addFaithPoints(output.get(Resource.FAITH));
             output.remove(Resource.FAITH);
         }
         player.getBoard().getDeposit().addMultipleToStrongbox(output);
     }
 
-    public void useLeaderProduction(Player player, LeaderCard card) {
-        //TODO Make sure the card is a production leader card
+    public void useLeaderProduction(Player player, LeaderCard card, Resource output) {
+        if (!gameController.isPlaying(player)) {
+            System.out.println("Not " + player.getNick() + "'s turn!");
+            return;
+        }
+        if(card == null) {
+            System.out.println("The requested card is not in the deck!");
+            return;
+        }
+        if(output == null || output == Resource.FAITH) {
+            System.out.println("Incorrect required resource type");
+            return;
+        }
+
+        if (!card.getSpecialAbility().getType().equals(SpecialAbilityType.PRODUCTION)) {
+            System.out.println("The selected leader does not have a production power!");
+        }
+
+        Resource cost = card.getSpecialAbility().getTargetResource();
+        if (player.getBoard().getDeposit().getAmountOfResource(cost) < 1) {
+            System.out.println(player.getNick() + " does not have enough " + cost);
+            return;
+        }
+
+        player.addFaithPoints(1);
+        player.getBoard().getDeposit().addToStrongbox(output);
     }
 
     public void selectInitialLeaders(Player player, List<LeaderCard> cards) {
+        if (cards.size() != 2) {
+            System.out.println("Wrong amount of selected leaders!");
+        }
 
+        try{
+            player.keepLeaders(cards);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/main/java/it/polimi/ingsw/controller/event/ProductionPlayerActionEvent.java
+++ src/main/java/it/polimi/ingsw/controller/event/ProductionPlayerActionEvent.java
@@ -3,6 +3,8 @@ package it.polimi.ingsw.controller.event;
 import it.polimi.ingsw.controller.GameController;
 import it.polimi.ingsw.model.Resource;
 import it.polimi.ingsw.model.card.Card;
+import it.polimi.ingsw.model.card.DevelopmentCard;
+import it.polimi.ingsw.model.card.LeaderCard;
 
 import java.io.Serial;
 import java.util.List;
@@ -13,6 +15,7 @@ public class ProductionPlayerActionEvent extends PlayerActionEvent {
     private static final long serialVersionUID = -1555283945398296364L;
 
     private UUID cardUUID;
+    private Resource leaderProductionDesiredResource;
     private List<Resource> baseProductionInput;
     private Resource baseProductionOutput;
 
@@ -21,9 +24,14 @@ public class ProductionPlayerActionEvent extends PlayerActionEvent {
         if(cardUUID == null) {
             controller.getPlayerController().useBaseProduction(getPlayer(controller), baseProductionInput, baseProductionOutput);
         } else {
-            Card card = controller.getGame().getDeck().getLeaderCardByUuid(cardUUID);
-            if(card == null) controller.getGame().getDeck().getDevelopmentCardByUuid(cardUUID);
-            controller.getPlayerController().useProduction(getPlayer(controller), card);
+            if (leaderProductionDesiredResource == null) {
+                DevelopmentCard card = controller.getGame().getDeck().getDevelopmentCardByUuid(cardUUID);
+                controller.getPlayerController().useDevelopmentProduction(getPlayer(controller), card);
+            }
+            else{
+                LeaderCard card = controller.getGame().getDeck().getLeaderCardByUuid(cardUUID);
+                controller.getPlayerController().useLeaderProduction(getPlayer(controller), card, leaderProductionDesiredResource);
+            }
         }
     }
 }
