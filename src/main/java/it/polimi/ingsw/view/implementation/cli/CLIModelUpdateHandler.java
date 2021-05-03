378cfa0f583adeb5278f8b34cc009db02d364242
Edoardo Spada <spada.s.edoardo@gmail.com>
2021-05-03 11:17:29 +0200

    chore: finalization of Javadoc for CliActionSender.

--- src/main/java/it/polimi/ingsw/view/implementation/cli/CLIModelUpdateHandler.java
+++ src/main/java/it/polimi/ingsw/view/implementation/cli/CLIModelUpdateHandler.java
@@ -88,12 +88,10 @@ public class CLIModelUpdateHandler extends ModelUpdateHandler {
 
     /**
      * Updates the turn of the given player. The states of the game are {@link GameState#CONNECTING},
-     * {@link GameState#CONNECTING}, {@link GameState#CONNECTING}, {@link GameState#CONNECTING},
-     * {@link GameState#CONNECTING}, {@link GameState#CONNECTING}, {@link GameState#CONNECTING}
+     * {@link GameState#PLAYING}, {@link GameState#SELECT_LEADERS}, {@link GameState#WAIT_SELECT_LEADERS},
+     * {@link GameState#STARTING}, {@link GameState#CHOOSING_NAME}, {@link GameState#CHOOSING_PLAYERS}.
      *
-     *
-     *
-     * @param playerName the name of the given
+     * @param playerName the name of the player to update the turn
      */
     @Override
     public void updateTurn(String playerName) {
@@ -114,6 +112,13 @@ public class CLIModelUpdateHandler extends ModelUpdateHandler {
         }
     }
 
+    /**
+     * Updates a row or column of the market with the resources given.
+     *
+     * @param index the index of the row or column to update
+     * @param changes the updated row or column of the market
+     * @param slideResource the resource to be put in the slide
+     */
     @Override
     public void updateMarket(int index, List<Resource> changes, Resource slideResource) {
         if (index >= 4) {
@@ -121,6 +126,14 @@ public class CLIModelUpdateHandler extends ModelUpdateHandler {
         } else getView().getModel().getMarket().updateMarketColumn(index, changes);
         getView().getModel().getMarket().setSlideResource(slideResource);
     }
+
+    /**
+     * Updates the deposit of a given player.
+     *
+     * @param playerName the name of the player whose deposit needs to be updated.
+     * @param changes a map representing the new deposit of the player after the updated is made
+     * @param leadersDeposit a map representing the new deposit of the leaders of the player after the updated is made
+     */
 
     @Override
     public void updateDeposit(String playerName, Map<Integer, List<Resource>> changes, Map<Integer, List<Resource>> leadersDeposit) {
@@ -135,6 +148,12 @@ public class CLIModelUpdateHandler extends ModelUpdateHandler {
         player.getDeposit().setLeadersDeposit(leadersDeposit);
     }
 
+    /**
+     * Updates the StrongBox of the given player.
+     *
+     * @param playerName the name of the player whose strongbox needs to be updated
+     * @param strongbox a Map representing the new strongbox of the player after the update is made
+     */
     @Override
     public void updateStrongbox(String playerName, Map<Resource, Integer> strongbox) {
         MockPlayer player = getView().getModel().getPlayer(playerName);
@@ -145,6 +164,12 @@ public class CLIModelUpdateHandler extends ModelUpdateHandler {
         player.getDeposit().setStrongbox(strongbox);
     }
 
+    /**
+     * Updates the faithPoints of a given player.
+     *
+     * @param playerName the name of the player whose faith points need to be updated
+     * @param faithPoints an integer representing the new number of faith points after the update is made
+     */
     @Override
     public void updateFaith(String playerName, int faithPoints) {
         MockPlayer player = getView().getModel().getPlayer(playerName);
@@ -154,6 +179,13 @@ public class CLIModelUpdateHandler extends ModelUpdateHandler {
 
         player.setFaithPoints(faithPoints);
     }
+
+    /**
+     * Updates the popeFavours of a given player.
+     *
+     * @param playerName the name of the player whose pope favours need to be updated
+     * @param popeFavours an integer representing the new number of pope favours after the update is made
+     */
 
     @Override
     public void updatePopeFavours(String playerName, int popeFavours) {
@@ -165,11 +197,23 @@ public class CLIModelUpdateHandler extends ModelUpdateHandler {
         player.setPopeFavours(popeFavours);
     }
 
+    /**
+     * Updates the chat.
+     *
+     * @param sender the name of the sender of the message
+     * @param message a string representing the message written by the player
+     */
     @Override
     public void updateChat(String sender, String message) {
         getView().getRenderer().printChatMessage(sender, message);
     }
 
+    /**
+     * Inserts the resources drawn from the market by a given player.
+     *
+     * @param playerName the name of the player whose drawn resources need to be stored
+     * @param result a list representing the resources to store
+     */
     @Override
     public void insertDrawnResources(String playerName, List<Resource> result) {
         MockPlayer player = getView().getModel().getPlayer(playerName);
