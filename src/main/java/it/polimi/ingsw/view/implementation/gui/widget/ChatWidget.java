01413df06ebad9812961c383d94ee88435c6e08a
Edoardo Spada <spada.s.edoardo@gmail.com>
2021-06-04 14:38:01 +0200

    chore: new design for chat.

--- src/main/java/it/polimi/ingsw/view/implementation/gui/widget/ChatWidget.java
+++ src/main/java/it/polimi/ingsw/view/implementation/gui/widget/ChatWidget.java
@@ -11,6 +11,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -18,7 +20,7 @@ import javafx.scene.layout.VBox;
 public class ChatWidget extends VBox {
     @FXML
     private VBox chat;
-    private Button button;
+    //   private Button button;
     private TextField textInput;
     private ScrollPane scrollPane;
 
@@ -34,24 +36,17 @@ public class ChatWidget extends VBox {
         setMaxWidth(150);
 
         scrollPane = new ScrollPane();
-        button = new Button();
         chat.setSpacing(10);
         chat.setPrefWidth(150);
         getChildren().add(scrollPane);
         textInput = new TextField();
         HBox hbox = new HBox();
-        hbox.getChildren().add(button);
-        button.getStyleClass().add("chat-button");
         hbox.getChildren().add(textInput);
         getChildren().add(hbox);
-
-
-        button.setOnAction(e -> {
-            if (!textInput.getText().isBlank()) {
-                button.setDisable(true);
+        textInput.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ENTER) {
                 GUI.instance().getActionSender().sendChatMessage(textInput.getText());
                 textInput.clear();
-                button.setDisable(false);
             }
         });
 
